ba3f6cb922b54ef2ba18d19ebb7f48814611c02c
Nicky Mar Collano <nmcollano@gmail.com>
2018-06-07 01:26:56 +0800

    Seed data to categories table

---
+++ Migrations/20180606172317_SeedCategories.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace marketmedia.Migrations
+{
+    public partial class SeedCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO dbo.Categories (Name) VALUES ('Health & Beauty')");
+            migrationBuilder.Sql("INSERT INTO dbo.Categories (Name) VALUES ('Electronice Devies')");
+            migrationBuilder.Sql("INSERT INTO dbo.Categories (Name) VALUES ('Fashion Accessories')");
+            migrationBuilder.Sql("INSERT INTO dbo.Categories (Name) VALUES ('Sports & Travel')");
+            migrationBuilder.Sql("INSERT INTO dbo.Categories (Name) VALUES ('Automotive & Motorcycles')");
+            migrationBuilder.Sql("INSERT INTO dbo.Categories (Name) VALUES ('Home & Lifestyle')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
