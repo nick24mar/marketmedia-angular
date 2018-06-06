72beb38b675a3904a9576d3c294138a553feee85
Nicky Mar Collano <nmcollano@gmail.com>
2018-06-06 23:57:47 +0800

    Add ItemsController

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -7,35 +7,35 @@ using Microsoft.AspNetCore.Mvc;
 namespace marketmedia.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class ItemsController : Controller
     {
-        // GET api/values
+        // GET api/items
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET api/items/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST api/items
         [HttpPost]
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/items/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/items/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
